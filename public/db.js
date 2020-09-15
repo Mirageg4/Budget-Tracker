d005dd7df0430b88495e75b902252e862a97c7e6
John Manouse <johnmanouse@Macintosh-2.local>
2020-09-15 11:40:11 -0400

    add db

---
+++ public/db.js
@@ -0,0 +1,23 @@
+//create budget database
+
+let db;
+const request = window.indexedDB.open("budget", 1);
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+
+    db.createObjectStore("pending", {
+        autoIncrement: true
+    });
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+//error function
+request.onerror = function(event) {
+    console.log(error);
+};
