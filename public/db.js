4929e6de040d2e3f955e85cb7de6b0bfe2959e3a
John Manouse <johnmanouse@Macintosh-2.local>
2020-09-15 12:35:10 -0400

    update db

--- public/db.js
+++ public/db.js
@@ -1,10 +1,10 @@
-//create budget database
-
+//create wrapper - budget database
 let db;
 const request = window.indexedDB.open("budget", 1);
 request.onupgradeneeded = function(event) {
     const db = event.target.result;
 
+    //Create Object Store
     db.createObjectStore("pending", {
         autoIncrement: true
     });
@@ -22,12 +22,20 @@ request.onerror = function(event) {
     console.log(error);
 };
 
+//Save item to Object Store
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const budgetStore = transaction.objectStore("pending");
+    budgetStore.add(record);  
+}
+
 //create pending transaction
 function checkDatabase() {
     const transaction = db.transaction(["pending"], "readwrite");
     const budgetStore = transaction.objectStore("pending");
     const getAll = budgetStore.getAll();
 
+    // Retrieve all objects in the index
     getAll.onsuccess = function() {
         if (getAll.result.length > 0) {
             fetch("/api/transaction/bulk", {
@@ -39,7 +47,19 @@ function checkDatabase() {
                 }
             })
 
-            
+            //Open transaction in pending db
+            .then(response => response.json())
+            .then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+                const budgetStore = transaction.objectStore("pending");
+               
+                //Clear stored items
+                budgetStore.clear();
+
+            });
         }
-    }
+    };
 }
+
+//Listener for app when back online
+window.addEventListener("online", checkDatabase);
